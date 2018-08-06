2a50990aa66cc6d7e4c01194f13705e80b880d0b
Alexandrea Oliveros (IS-PH) <alexandrea_oliveros@trendmicro.com>
2018-08-07 01:12:17 +0800

    Reconfigured clock fo another test run

--- clock.py
+++ clock.py
@@ -4,14 +4,18 @@ from hello.views import slack_bot, get_trending
 
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('interval', minutes=1)
-# def timed_job():
-#     views.postTrend("CC06NGT8S", views.trends())
-#     # print('This job is run every 30 minute.')
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    slack_bot.api_call(
+         "chat.postMessage",
+         channel="CBXF4AC7J",
+         text=get_trending(),
+         icon_emoji=':robot_face:'
+     )
 
 
 # @sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hours=8)
 def scheduled_job():
     slack_bot.api_call(
         "chat.postMessage",
