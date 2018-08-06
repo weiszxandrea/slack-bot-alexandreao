e4721109fbdbceece25a2a16effb1f201d6a4b35
Alexandrea Oliveros (IS-PH) <alexandrea_oliveros@trendmicro.com>
2018-08-06 22:01:15 +0800

    Added definitions for getChannel and getTrending functions

--- hello/views.py
+++ hello/views.py
@@ -1,17 +1,29 @@
 import json
 import os
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+
+def getTrending():
+    pass
+
+
+def getChannel(request):
+    ch_event = json.loads(request.body)
+    token = ch_event['token']
+    channel = ch_event['event']['channel']
+    event_text = ch_event['event']['text']
+    if "trend" in event_text or "twitter" in event_text:
+        pass
+
+    #return HttpResponse(ch_event, content_type="text/plain")
 
 @csrf_exempt
 def slack(request):
     req = json.loads(request.body)
     token = req['token']
     if os.environ.get("verification_token") == token:
-        challenge = req['challenge']
+        getChannel(request)
     else:
-        challenge = "Empty challenge"
-
-    return HttpResponse(challenge, content_type="text/plain")
-
+        pass
+    #return HttpResponse("", content_type="text/plain")
