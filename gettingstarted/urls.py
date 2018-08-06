f3cb944e13d2d7e4279dd4962fcff5d792d8685f
Alexandrea Oliveros (IS-PH) <alexandrea_oliveros@trendmicro.com>
2018-08-06 21:07:33 +0800

    Added request challenge function definition

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,12 +6,6 @@ admin.autodiscover()
 
 import hello.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    path('admin/', admin.site.urls),
+    url(r'^slack-bot-alexandreao', hello.views.slack, name='slack'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,17 @@
+import os
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-from .models import Greeting
+@csrf_exempt
+def slack(request):
+    req = json.loads(request.body)
+    token = req['token']
+    if os.environ.get("verification_key") == token:
+        challenge = req['challenge']
+    else:
+        challenge = "Empty challenge"
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
+    return HttpResponse(challenge, content_type="text/plain")
 
