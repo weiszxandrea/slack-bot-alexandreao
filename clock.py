2458934779248b349a4d630661aa852e18b895bc
Alexandrea Oliveros (IS-PH) <alexandrea_oliveros@trendmicro.com>
2018-08-07 01:00:59 +0800

    Configured clock for test run

--- clock.py
+++ clock.py
@@ -1,5 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from hello import views
+from hello.views import slack_bot, get_trending
+
 sched = BlockingScheduler()
 
 # @sched.scheduled_job('interval', minutes=1)
@@ -7,9 +9,16 @@ sched = BlockingScheduler()
 #     views.postTrend("CC06NGT8S", views.trends())
 #     # print('This job is run every 30 minute.')
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
+
+# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
+@sched.scheduled_job('interval', minutes=1)
 def scheduled_job():
-    views.postTrend("CC06NGT8S", views.trends())
-    # print('This job is run every weekday at 5pm.')
+    slack_bot.api_call(
+        "chat.postMessage",
+        channel="CBXF4AC7J",
+        text=get_trending(),
+        icon_emoji=':robot_face:'
+    )
+
 
 sched.start()
