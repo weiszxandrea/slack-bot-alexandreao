396bd85f8811b97194f41f9526a2ece095a7042a
Alexandrea Oliveros (IS-PH) <alexandrea_oliveros@trendmicro.com>
2018-08-07 01:20:13 +0800

    Configured clock to post every 8 am everyday

--- clock.py
+++ clock.py
@@ -1,9 +1,8 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from hello import views
 from hello.views import slack_bot, get_trending
 
 sched = BlockingScheduler()
-
+"""
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     slack_bot.api_call(
@@ -12,9 +11,9 @@ def timed_job():
          text=get_trending(),
          icon_emoji=':robot_face:'
      )
+"""
 
 
-# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
 @sched.scheduled_job('cron', day_of_week='mon-sun', hours=8)
 def scheduled_job():
     slack_bot.api_call(
